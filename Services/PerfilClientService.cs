f5aec95c2ecc5cd57c065da96dac3acf3e46139f
LuisAlonsoAndradeLopez <caffeinated340@gmail.com>
2024-05-28 00:45:35 -0600

    Adding Files

--- Services/PerfilClientService.cs
+++
@@ -1,9 +0,0 @@
-namespace frontendnet.Services;
-
-public class PerfilClientService(HttpClient client)
-{
-    public async Task<string> ObtenTiempoAsync()
-    {
-        return await client.GetStringAsync($"api/auth/tiempo");
-    }
-}
